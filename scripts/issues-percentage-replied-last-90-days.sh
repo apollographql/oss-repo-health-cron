20b5d4e153adb421de492866173d64de4c065c55
Alessia Bellisario <github@bellisar.io>
2024-01-19 13:07:26 -0500

    chore: run Kotlin and iOS workflows

--- scripts/issues-percentage-replied-last-90-days.sh
+++ scripts/issues-percentage-replied-last-90-days.sh
@@ -4,9 +4,6 @@ ISSUES_CREATED_LAST_90_DAYS=$(gh issue list --search "${FILTERED_OUT_LABELS} ${F
 NUM_ISSUES_CREATED=$(jq '. | length' <<< $ISSUES_CREATED_LAST_90_DAYS)
 NUM_ISSUES_REPLIED_TO=0
 
-echo "Issues created last 90 days: ${ISSUES_CREATED_LAST_90_DAYS}"
-echo "Num issues created last 90 days: ${NUM_ISSUES_CREATED}"
-
 for issue in $(echo -e $ISSUES_CREATED_LAST_90_DAYS | jq -s 'flatten(1)')
 do
   if [ "$issue" = "[" ] || [ "$issue" = "]" ]; then
@@ -16,22 +13,13 @@ do
 
     ISSUE_OPENED_DATE=$(jq '. | .createdAt' <<< $ISSUE_DETAILS)
 
-    echo "Issue number: ${issue//,}"
-    echo "Issue opened date: ${ISSUE_OPENED_DATE}"
-
     # get relative datetime 3 days after issue opened
     THREE_DAYS_AFTER_ISSUE_OPENED=$(date -j -v+3d -f "%Y-%m-%dT%H:%M:%SZ" "${ISSUE_OPENED_DATE//\"}" "+%Y-%m-%dT%H:%M:%SZ")
-
-    echo "3 days after issue opened: ${THREE_DAYS_AFTER_ISSUE_OPENED}"
-
-    echo $ISSUE_DETAILS
 
     # get number of comments by maintainers within the first 3 days
     # NB: manually filter out apollo-cla bot, but otherwise include all users
     # with CONTRIBUTOR association
     ISSUE_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_ISSUE_OPENED" '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .createdAt <= $DATE)] | length' <<< $ISSUE_DETAILS)
-
-    echo "Issue comments: ${ISSUE_COMMENTS}"
 
     if [ "$ISSUE_COMMENTS" = "0" ]; then
       echo "Issue without a reply in 72 hours: ${issue//,}"
--- scripts/prs-percentage-replied-last-90-days.sh
+++ scripts/prs-percentage-replied-last-90-days.sh
@@ -4,9 +4,6 @@ PRS_CREATED_LAST_90_DAYS=$(gh pr list --search "${FILTERED_OUT_LABELS} ${FILTERE
 NUM_PRS_CREATED=$(jq '. | length' <<< $PRS_CREATED_LAST_90_DAYS)
 NUM_PRS_REPLIED_TO=0
 
-echo "PRs created last 90 days: ${PRS_CREATED_LAST_90_DAYS}"
-echo "Num PRs created last 90 days: ${NUM_PRS_CREATED}"
-
 for pr in $(echo -e $PRS_CREATED_LAST_90_DAYS | jq -s 'flatten(1)')
 do
   if [ "$pr" = "[" ] || [ "$pr" = "]" ]; then
@@ -16,20 +13,13 @@ do
 
     PR_OPENED_DATE=$(jq '. | .createdAt' <<< $PR_DETAILS)
 
-    echo "Issue number: ${pr//,}"
-    echo "Issue opened date: ${PR_OPENED_DATE}"
-
     # get relative datetime 3 days after issue opened
     THREE_DAYS_AFTER_PR_OPENED=$(date -j -v+3d -f "%Y-%m-%dT%H:%M:%SZ" "${PR_OPENED_DATE//\"}" "+%Y-%m-%dT%H:%M:%SZ")
-
-    echo "3 days after PR opened: ${THREE_DAYS_AFTER_PR_OPENED}"
 
     # get number of comments by maintainers within the first 3 days
     # NB: manually filter out apollo-cla bot, but otherwise include all users
     # with CONTRIBUTOR association
     PR_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .createdAt <= $DATE)] | length' <<< $PR_DETAILS)
-
-    echo "PR comments: ${PR_COMMENTS}"
 
     if [ "$PR_COMMENTS" = "0" ]; then
       echo "PR without a reply in 72 hours: ${pr//,}"
