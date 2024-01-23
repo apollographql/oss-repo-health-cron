64c044362480e440ed632426f6b33318435bbc98
Alessia Bellisario <github@bellisar.io>
2024-01-23 14:38:32 -0500

    chore: some refactoring

--- scripts/issues-closed-last-90-days.sh
+++ scripts/issues-closed-last-90-days.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ISSUES_CLOSED_LAST_90_DAYS=$(gh issue list --search "closed:${DATE_90_DAYS_AGO}..${DATE_TODAY}" --limit ${LIMIT} --json author --jq 'length')
-ISSUES_CLOSED_PREV_90_DAYS=$(gh issue list --search "closed:${DATE_181_DAYS_AGO}..${DATE_91_DAYS_AGO}" --limit ${LIMIT} --json author --jq 'length')
+ISSUES_CLOSED_LAST_90_DAYS=$(gh issue list --search "closed:${DATE_90_DAYS_AGO}..${DATE_TODAY}" --limit 1000 --json author --jq 'length')
+ISSUES_CLOSED_PREV_90_DAYS=$(gh issue list --search "closed:${DATE_181_DAYS_AGO}..${DATE_91_DAYS_AGO}" --limit 1000 --json author --jq 'length')
 ISSUES_CLOSED_PERCENTAGE_CHANGE=$(bc <<< "scale=2; ($ISSUES_CLOSED_LAST_90_DAYS-$ISSUES_CLOSED_PREV_90_DAYS)/$ISSUES_CLOSED_PREV_90_DAYS * 100")
 
-echo "ISSUES_CLOSED_LAST_90_DAYS=${ISSUES_CLOSED_LAST_90_DAYS}" >> "$GITHUB_ENV"
-echo "ISSUES_CLOSED_PREV_90_DAYS=${ISSUES_CLOSED_PREV_90_DAYS}" >> "$GITHUB_ENV"
-echo "ISSUES_CLOSED_PERCENTAGE_CHANGE=${ISSUES_CLOSED_PERCENTAGE_CHANGE}" >> "$GITHUB_ENV"
+echo "Issues closed in last 90 days (${DATE_TODAY}-${DATE_90_DAYS_AGO}): ${ISSUES_CLOSED_LAST_90_DAYS}"
+echo "Issues closed in previous 90 day period (${DATE_91_DAYS_AGO}-${DATE_181_DAYS_AGO}): ${ISSUES_CLOSED_PREV_90_DAYS}"
+echo "Percentage change: ${ISSUES_CLOSED_PERCENTAGE_CHANGE}
