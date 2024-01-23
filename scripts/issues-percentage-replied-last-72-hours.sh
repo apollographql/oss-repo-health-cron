cd12bb8a1e31d8c60732a99d7e4a27ef6450cf60
Alessia Bellisario <github@bellisar.io>
2024-01-23 15:13:58 -0500

    fix: also filter out if there are any review comments

--- scripts/issues-percentage-replied-last-72-hours.sh
+++ scripts/issues-percentage-replied-last-72-hours.sh
@@ -4,33 +4,34 @@ ISSUES_CREATED_LAST_3_DAYS=$(gh issue list --search "${FILTERED_OUT_LABELS} ${FI
 ISSUES_SLACK_BOT_MESSAGE=""
 NUM_ISSUES_TO_REPLY_TO=0
 
-for issue in $(echo -e $ISSUES_CREATED_LAST_3_DAYS | jq -s 'flatten(1)')
-do
-  if [ "$issue" = "[" ] || [ "$issue" = "]" ]; then
-    :
-  else
-    ISSUE_DETAILS=$(gh issue view ${issue//,} --json comments,url)
-    ISSUE_URL=$(jq '. | .url' <<< $ISSUE_DETAILS)
-    
-    echo $(jq '. | .comments' <<< $ISSUE_DETAILS)
-    # get number of comments by maintainers within the first 3 days
-    # NB: manually filter out apollo-cla bot, but otherwise include all users
-    # with CONTRIBUTOR association
-    ISSUE_COMMENTS=$(jq '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions")] | length' <<< $ISSUE_DETAILS)
-
-    if [ "$ISSUE_COMMENTS" = "0" ]; then
-      ISSUES_SLACK_BOT_MESSAGE="${ISSUES_SLACK_BOT_MESSAGE}\n*<${ISSUE_URL//\"}|${issue//,}>*"
-      echo "Issue without a reply in 72 hours: ${issue//,}"
-      NUM_ISSUES_TO_REPLY_TO=$((NUM_ISSUES_TO_REPLY_TO+1))
+if [ "$ISSUES_CREATED_LAST_3_DAYS" != [] ]; then
+  for issue in $(echo -e $ISSUES_CREATED_LAST_3_DAYS | jq -s 'flatten(1)')
+  do
+    if [ "$issue" = "[" ] || [ "$issue" = "]" ]; then
+      :
     else
-      echo "Issue with a reply in 72 hours: ${issue//,}"
-    fi
+      ISSUE_DETAILS=$(gh issue view ${issue//,} --json comments,url)
+      ISSUE_URL=$(jq '. | .url' <<< $ISSUE_DETAILS)
+      
+      echo $(jq '. | .comments' <<< $ISSUE_DETAILS)
+      # get number of comments by maintainers within the first 3 days
+      # NB: manually filter out apollo-cla bot, but otherwise include all users
+      # with CONTRIBUTOR association
+      ISSUE_COMMENTS=$(jq '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions")] | length' <<< $ISSUE_DETAILS)
 
-
-    if [ "$NUM_ISSUES_TO_REPLY_TO" = "0" ]; then
-      ISSUES_SLACK_BOT_MESSAGE="\nAll caught up :tada:"
+      if [ "$ISSUE_COMMENTS" = "0" ]; then
+        ISSUES_SLACK_BOT_MESSAGE="${ISSUES_SLACK_BOT_MESSAGE}\n*<${ISSUE_URL//\"}|${issue//,}>*"
+        echo "Issue without a reply in 72 hours: ${issue//,}"
+        NUM_ISSUES_TO_REPLY_TO=$((NUM_ISSUES_TO_REPLY_TO+1))
+      else
+        echo "Issue with a reply in 72 hours: ${issue//,}"
+      fi
     fi
-  fi
-done
+  done
+fi
+
+if [ "$NUM_ISSUES_TO_REPLY_TO" = "0" ]; then
+  ISSUES_SLACK_BOT_MESSAGE="\nAll caught up :tada:"
+fi
 
 echo "ISSUES_SLACK_BOT_MESSAGE=${ISSUES_SLACK_BOT_MESSAGE}" >> "$GITHUB_OUTPUT"
--- scripts/issues-percentage-replied-last-90-days.sh
+++ scripts/issues-percentage-replied-last-90-days.sh
@@ -4,31 +4,33 @@ ISSUES_CREATED_LAST_90_DAYS=$(gh issue list --search "${FILTERED_OUT_LABELS} ${F
 NUM_ISSUES_CREATED=$(jq '. | length' <<< $ISSUES_CREATED_LAST_90_DAYS)
 NUM_ISSUES_REPLIED_TO=0
 
-for issue in $(echo -e $ISSUES_CREATED_LAST_90_DAYS | jq -s 'flatten(1)')
-do
-  if [ "$issue" = "[" ] || [ "$issue" = "]" ]; then
-    :
-  else
-    ISSUE_DETAILS=$(gh issue view ${issue//,} --json createdAt,comments)
-
-    ISSUE_OPENED_DATE=$(jq '. | .createdAt' <<< $ISSUE_DETAILS)
-
-    # get relative datetime 3 days after issue opened
-    THREE_DAYS_AFTER_ISSUE_OPENED=$(date -j -v+3d -f "%Y-%m-%dT%H:%M:%SZ" "${ISSUE_OPENED_DATE//\"}" "+%Y-%m-%dT%H:%M:%SZ")
-
-    # get number of comments by maintainers within the first 3 days
-    # NB: manually filter out apollo-cla bot, but otherwise include all users
-    # with CONTRIBUTOR association
-    ISSUE_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_ISSUE_OPENED" '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions" and .createdAt <= $DATE)] | length' <<< $ISSUE_DETAILS)
-
-    if [ "$ISSUE_COMMENTS" = "0" ]; then
-      echo "Issue without a reply in 72 hours: ${issue//,}"
+if [ "$ISSUES_CREATED_LAST_90_DAYS" != [] ]; then
+  for issue in $(echo -e $ISSUES_CREATED_LAST_90_DAYS | jq -s 'flatten(1)')
+  do
+    if [ "$issue" = "[" ] || [ "$issue" = "]" ]; then
+      :
     else
-      echo "Issue with a reply in 72 hours: ${issue//,}"
-      NUM_ISSUES_REPLIED_TO=$((NUM_ISSUES_REPLIED_TO+1))
+      ISSUE_DETAILS=$(gh issue view ${issue//,} --json createdAt,comments)
+
+      ISSUE_OPENED_DATE=$(jq '. | .createdAt' <<< $ISSUE_DETAILS)
+
+      # get relative datetime 3 days after issue opened
+      THREE_DAYS_AFTER_ISSUE_OPENED=$(date -j -v+3d -f "%Y-%m-%dT%H:%M:%SZ" "${ISSUE_OPENED_DATE//\"}" "+%Y-%m-%dT%H:%M:%SZ")
+
+      # get number of comments by maintainers within the first 3 days
+      # NB: manually filter out apollo-cla bot, but otherwise include all users
+      # with CONTRIBUTOR association
+      ISSUE_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_ISSUE_OPENED" '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions" and .createdAt <= $DATE)] | length' <<< $ISSUE_DETAILS)
+
+      if [ "$ISSUE_COMMENTS" = "0" ]; then
+        echo "Issue without a reply in 72 hours: ${issue//,}"
+      else
+        echo "Issue with a reply in 72 hours: ${issue//,}"
+        NUM_ISSUES_REPLIED_TO=$((NUM_ISSUES_REPLIED_TO+1))
+      fi
     fi
-  fi
-done
+  done
+fi
 
 PERCENTAGE_ISSUES_REPLIED_TO=$(bc <<< "scale=2; ($NUM_ISSUES_REPLIED_TO/$NUM_ISSUES_CREATED) * 100")
 echo "Issues replied to within 72 hours: ${PERCENTAGE_ISSUES_REPLIED_TO}%"
