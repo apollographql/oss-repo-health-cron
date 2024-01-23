d29468f59f55d75cc5f5591cb0a9214ef47b751f
Alessia Bellisario <github@bellisar.io>
2024-01-23 15:56:44 -0500

    fix: also filter out review comments for 90 day stat

--- scripts/prs-percentage-replied-last-90-days.sh
+++ scripts/prs-percentage-replied-last-90-days.sh
@@ -15,13 +15,18 @@ do
 
     # get relative datetime 3 days after issue opened
     THREE_DAYS_AFTER_PR_OPENED=$(date -j -v+3d -f "%Y-%m-%dT%H:%M:%SZ" "${PR_OPENED_DATE//\"}" "+%Y-%m-%dT%H:%M:%SZ")
+    
+    PR_REVIEW_COMMENTS=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/apollographql/${REPOSITORY}/pulls/${pr//,}/comments")
+    
+    # Note the casing of authorAssociation/author_association and createdAt/created_at differs between gh CLI calls and REST API calls
+    PR_PREVIEW_COMMENTS_BY_MAINTAINERS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '[.[] | select(.author_association == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions" and .created_at <= $DATE)] | length' <<< $PR_REVIEW_COMMENTS)
 
     # get number of comments by maintainers within the first 3 days
     # NB: manually filter out apollo-cla bot, but otherwise include all users
     # with CONTRIBUTOR association
     PR_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '. | [.comments[] | select(.authorAssociation == "CONTRIBUTOR" and .author.login != "apollo-cla" and .author.login != "github-actions" and .createdAt <= $DATE)] | length' <<< $PR_DETAILS)
 
-    if [ "$PR_COMMENTS" = "0" ]; then
+    if [ "$PR_COMMENTS" = "0" ] && [ "$PR_PREVIEW_COMMENTS_BY_MAINTAINERS" = "0" ]; then
       echo "PR without a reply in 72 hours: ${pr//,}"
     else
       echo "PR with a reply in 72 hours: ${pr//,}"
