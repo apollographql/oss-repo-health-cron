3bc4d4e28282d0fbced4f4ff0f87609224894a07
Alessia Bellisario <github@bellisar.io>
2024-01-24 12:12:31 -0500

    chore: use scale=1

--- scripts/issues-closed-last-90-days.sh
+++ scripts/issues-closed-last-90-days.sh
@@ -2,7 +2,7 @@
 
 ISSUES_CLOSED_LAST_90_DAYS=$(gh issue list --search "closed:${DATE_90_DAYS_AGO}..${DATE_TODAY}" --limit 1000 --json author --jq 'length')
 ISSUES_CLOSED_PREV_90_DAYS=$(gh issue list --search "closed:${DATE_181_DAYS_AGO}..${DATE_91_DAYS_AGO}" --limit 1000 --json author --jq 'length')
-ISSUES_CLOSED_PERCENTAGE_CHANGE=$(bc <<< "scale=2; ($ISSUES_CLOSED_LAST_90_DAYS-$ISSUES_CLOSED_PREV_90_DAYS)/$ISSUES_CLOSED_PREV_90_DAYS * 100")
+ISSUES_CLOSED_PERCENTAGE_CHANGE=$(bc <<< "scale=1; ($ISSUES_CLOSED_LAST_90_DAYS-$ISSUES_CLOSED_PREV_90_DAYS)/$ISSUES_CLOSED_PREV_90_DAYS * 100")
 
 echo "Issues closed in last 90 days (${DATE_TODAY}-${DATE_90_DAYS_AGO}): ${ISSUES_CLOSED_LAST_90_DAYS}"
 echo "Issues closed in previous 90 day period (${DATE_91_DAYS_AGO} to ${DATE_181_DAYS_AGO}): ${ISSUES_CLOSED_PREV_90_DAYS}"
