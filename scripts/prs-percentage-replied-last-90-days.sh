b24fa6edd40975dd5e18312b43e4e3cc267f4a2d
Alessia Bellisario <github@bellisar.io>
2024-05-20 14:25:41 -0400

    fix: separately fetch PR reviews which are distinct from review comments and comments

--- scripts/prs-percentage-replied-last-90-days.sh
+++ scripts/prs-percentage-replied-last-90-days.sh
@@ -22,17 +22,22 @@ else
       # get relative datetime 3 days after issue opened
       THREE_DAYS_AFTER_PR_OPENED=$(date -u -d"${PR_OPENED_DATE//\"} +3 days" "+%Y-%m-%dT%H:%M:%SZ")
       
+      # PR reviews, review comments and comments must be fetched separately:
+      # - if a maintainer reviews a PR (using the review UI) and leaves comments without either approving or requesting changes,
+      #   they will be returned by the /pulls/:id/comments endpoint
       PR_REVIEW_COMMENTS=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/apollographql/${REPOSITORY}/pulls/${pr//,}/comments")
+      # - if a PR is approved with or without a comment, it will appear under the /pulls/:id/reviews endpoint
+      PR_REVIEWS=$(gh api -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "/repos/apollographql/${REPOSITORY}/pulls/${pr//,}/reviews")
       
       # Note the casing of authorAssociation/author_association and createdAt/created_at differs between gh CLI calls and REST API calls
       PR_REVIEW_COMMENTS_BY_MAINTAINERS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '[.[] | select((.author_association == "CONTRIBUTOR" or .author_association == "MEMBER") and .author.login != "apollo-cla" and .author.login != "netlify" and .author.login != "github-actions" and .created_at <= $DATE)] | length' <<< $PR_REVIEW_COMMENTS)
-
-      # get number of comments by maintainers within the first 3 days
-      # NB: manually filter out apollo-cla bot, but otherwise include all users
-      # with CONTRIBUTOR association
+      PR_REVIEWS_BY_MAINTAINERS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '[.[] | select((.author_association == "CONTRIBUTOR" or .author_association == "MEMBER") and .author.login != "apollo-cla" and .author.login != "netlify" and .author.login != "github-actions" and .created_at <= $DATE)] | length' <<< $PR_REVIEWS)
+      
+      # - if a comment if left on a PR without a review, it will appear under the comments key in the response from `gh pr view`, which we parse below
+      # get number of comments by org members within the first 3 days
       PR_COMMENTS=$(jq --arg DATE "$THREE_DAYS_AFTER_PR_OPENED" '. | [.comments[] | select((.authorAssociation == "CONTRIBUTOR" or .authorAssociation == "MEMBER") and .author.login != "apollo-cla" and .author.login != "netlify" and .author.login != "github-actions" and .createdAt <= $DATE)] | length' <<< $PR_DETAILS)
 
-      if [ "$PR_COMMENTS" = "0" ] && [ "$PR_REVIEW_COMMENTS_BY_MAINTAINERS" = "0" ]; then
+      if [ "$PR_COMMENTS" = "0" ] && [ "$PR_REVIEW_COMMENTS_BY_MAINTAINERS" = "0" ] && [ "$PR_REVIEWS_BY_MAINTAINERS" = "0" ]; then
         echo "PR without a reply in 72 hours: ${pr//,}"
       else
         echo "PR with a reply in 72 hours: ${pr//,}"
